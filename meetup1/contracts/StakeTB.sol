751be08dee5e6db100e8c69511f24a047933b4d6
eyusufatik <esadyusufatik@gmail.com>
2022-04-25 23:08:19 +0300

    end of meet commit

--- meetup1/contracts/StakeTB.sol
+++ meetup1/contracts/StakeTB.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 contract StakeTB{
     IERC20 _token;
     mapping(address => stakeInfo) _addressToStakeInfo;
-    uint256 _rewardRatePerSecond = 1 * 10**16;
+    uint256 _rewardRatePerMinute = 1 * 10**16;
 
 
     struct stakeInfo{
@@ -22,31 +22,36 @@ contract StakeTB{
 
     function stake(uint256 amount) public {
         stakeInfo memory oldInfo = _addressToStakeInfo[msg.sender];
+
         if (oldInfo.amount > 0){
             uint256 rewardAmount = calcReward(oldInfo);
             _token.transfer(msg.sender, rewardAmount);
         }
-        // emit event
+
+        // TODO emit event
         _token.transferFrom(msg.sender, address(this), amount);
         stakeInfo memory newInfo = stakeInfo(amount + oldInfo.amount, block.timestamp);
         _addressToStakeInfo[msg.sender] = newInfo;
     }
 
     function harvest() public {
-        stakeInfo memory info = _addressToStakeInfo[msg.sender];
+        stakeInfo storage info = _addressToStakeInfo[msg.sender];
         uint256 rewardAmount = calcReward(info);
+        info.startTime = block.timestamp;
         _token.transfer(msg.sender, rewardAmount);
     }
 
     function calcReward(stakeInfo memory info) internal view returns(uint256){
-        return info.amount * _rewardRatePerSecond * (block.timestamp - info.startTime) / 60;
+        return info.amount * _rewardRatePerMinute * (block.timestamp - info.startTime) / 60;
     }
 
     function unstake() public {
         stakeInfo memory info = _addressToStakeInfo[msg.sender];
         require(info.amount > 0, "You haven't staked any coins yet.");
-
+        stakeInfo memory newInfo = stakeInfo(0, 0);
+        _addressToStakeInfo[msg.sender] = newInfo;
         _token.transfer(msg.sender, info.amount + calcReward(info));
+
     }
 
     function stakedAmount() public view returns(uint256){
--- meetup1/contracts/TokenBlack.sol
+++ meetup1/contracts/TokenBlack.sol
@@ -5,7 +5,14 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TokenBlack is ERC20{
+    address _owner;
     constructor(uint256 initialSupply) ERC20("TokenBlack", "TB"){
+        _owner = msg.sender;
         _mint(msg.sender, initialSupply*10**18);
+    }
+
+    function mintTokens(uint256 amount, address to) public {
+        require(msg.sender == _owner);
+        _mint(to, amount * 10**18);
     }
 }
